8cdf1db6a8e51a140041026feecc9f32ae213c48
francisco-solis99 <franciscosoliscss@gmail.com>
2024-12-06 18:09:02 -0600

    [fix] the solution for challenge 03 in TS

--- typeScript/challenge03.ts
+++ typeScript/challenge03.ts
@@ -1,21 +1,53 @@
-function organizeInventory(inventory) {
-  if(!inventory.length) return {}
-  const organizedInventary = {}
+type Inventory = Array<
+  { name: string, quantity: number, category: string }
+>
 
-  inventory.forEach(item => {
-   const {name, category, quantity} = item
-   if(!organizedInventary[category]) {
-       organizedInventary[category] = {
-         [name]: quantity
-       }
-       return
-   }
-
-   organizedInventary[category] = {
-     ...organizedInventary[category],
-     [name]: quantity + (organizedInventary[category][name] ?? 0)
-   }
-  })
-
-  return organizedInventary
+function organizeInventory(inventory: Inventory): object {
+  const invetoryOrganized: Record<string, Record<string, number>> = {};
+  for(const {name, category, quantity} of inventory) {
+    if(!invetoryOrganized[category]) {
+      invetoryOrganized[category] = {}
+    }
+    invetoryOrganized[category][name] = (invetoryOrganized[category][name] ?? 0) + quantity;
+  }
+  return invetoryOrganized
 }
+
+const inventory = [
+  { name: 'doll', quantity: 5, category: 'toys' },
+  { name: 'car', quantity: 3, category: 'toys' },
+  { name: 'ball', quantity: 2, category: 'sports' },
+  { name: 'car', quantity: 2, category: 'toys' },
+  { name: 'racket', quantity: 4, category: 'sports' }
+]
+
+organizeInventory(inventory)
+
+// Expected result:
+// {
+//   toys: {
+//     doll: 5,
+//     car: 5
+//   },
+//   sports: {
+//     ball: 2,
+//     racket: 4
+//   }
+
+const inventory2 = [
+  { name: 'book', quantity: 10, category: 'education' },
+  { name: 'book', quantity: 5, category: 'education' },
+  { name: 'paint', quantity: 3, category: 'art' }
+]
+
+organizeInventory(inventory2)
+
+// Expected result:
+// {
+//   education: {
+//     book: 15
+//   },
+//   art: {
+//     paint: 3
+//   }
+// }
