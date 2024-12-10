d9641caafcfd102c38dec944751d4976e56c0ca6
francisco-solis99 <franciscosoliscss@gmail.com>
2024-12-10 14:24:45 -0600

    [test-TS] add the tests for challenge03

--- typeScript/challenges/challenge03.ts
+++ typeScript/challenges/challenge03.ts
@@ -2,7 +2,7 @@ type Inventory = Array<
 { name: string, quantity: number, category: string }
 >
 
-function organizeInventory (inventory: Inventory): object {
+export function organizeInventory (inventory: Inventory): object {
   const invetoryOrganized: Record<string, Record<string, number>> = {}
   for (const { name, category, quantity } of inventory) {
     if (invetoryOrganized[category] === undefined) {
@@ -12,42 +12,3 @@ function organizeInventory (inventory: Inventory): object {
   }
   return invetoryOrganized
 }
-
-const inventory = [
-  { name: 'doll', quantity: 5, category: 'toys' },
-  { name: 'car', quantity: 3, category: 'toys' },
-  { name: 'ball', quantity: 2, category: 'sports' },
-  { name: 'car', quantity: 2, category: 'toys' },
-  { name: 'racket', quantity: 4, category: 'sports' }
-]
-
-organizeInventory(inventory)
-
-// Expected result:
-// {
-//   toys: {
-//     doll: 5,
-//     car: 5
-//   },
-//   sports: {
-//     ball: 2,
-//     racket: 4
-//   }
-
-const inventory2 = [
-  { name: 'book', quantity: 10, category: 'education' },
-  { name: 'book', quantity: 5, category: 'education' },
-  { name: 'paint', quantity: 3, category: 'art' }
-]
-
-organizeInventory(inventory2)
-
-// Expected result:
-// {
-//   education: {
-//     book: 15
-//   },
-//   art: {
-//     paint: 3
-//   }
-// }
---
+++ typeScript/tests/challenge03.test.ts
@@ -0,0 +1,52 @@
+import { organizeInventory } from '../challenges/challenge03.ts'
+import { describe, test, expect } from '@jest/globals'
+
+describe('Organize Inventory', () => {
+  test('Organize the gifts - Test 1', () => {
+    const inventory = [
+      { name: 'doll', quantity: 5, category: 'toys' },
+      { name: 'car', quantity: 3, category: 'toys' },
+      { name: 'ball', quantity: 2, category: 'sports' },
+      { name: 'car', quantity: 2, category: 'toys' },
+      { name: 'racket', quantity: 4, category: 'sports' }
+    ]
+    expect(organizeInventory(inventory)).toEqual({
+      toys: {
+        doll: 5,
+        car: 5
+      },
+      sports: {
+        ball: 2,
+        racket: 4
+      }
+    })
+  })
+  test('Organize the gifts - Test 2', () => {
+    const inventory2 = [
+      { name: 'book', quantity: 10, category: 'education' },
+      { name: 'book', quantity: 5, category: 'education' },
+      { name: 'paint', quantity: 3, category: 'art' }
+    ]
+    expect(organizeInventory(inventory2)).toEqual({
+      education: {
+        book: 15
+      },
+      art: {
+        paint: 3
+      }
+    })
+  })
+  test('Organize the gifts - Test 3', () => {
+    expect(organizeInventory([])).toEqual({})
+  })
+  test('Organize the gifts - Test 4', () => {
+    const inventory = [
+      { name: 'doll', quantity: 5, category: 'toys' }
+    ]
+    expect(organizeInventory(inventory)).toEqual({
+      toys: {
+        doll: 5
+      }
+    })
+  })
+})
