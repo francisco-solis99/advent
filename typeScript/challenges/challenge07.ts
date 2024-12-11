83dae0687d9c5c79a96432498e7941d762f75752
francisco-solis99 <franciscosoliscss@gmail.com>
2024-12-10 21:11:29 -0600

    [chore-TS] add another solution for challenge07 using recursion

--- typeScript/challenges/challenge07.ts
+++ typeScript/challenges/challenge07.ts
@@ -8,3 +8,12 @@ export function fixPackages (packages: string): string {
   }
   return fixedPackages
 }
+
+//  Other solution using recursion (less optimized)
+export function fixPackages2 (packages: string): string {
+  const fixedPackages: string = packages.replace(/\(([^()]+)\)/g, (_, match) => {
+    return match.split('').reverse().join('')
+  })
+  if (fixedPackages.includes('(')) return fixPackages(fixedPackages)
+  return fixedPackages
+}
